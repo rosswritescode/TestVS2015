8b817c1d950d5a18195225b1b2bb1de63baad7c5
Ross <ross@ross.ross>
2015-08-11 10:36:26 +0100

    Made small change

---
+++ TestWeb2/Areas/NPM/NPMAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TestWeb2.Areas.NPM
+{
+    public class NPMAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "NPM";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "NPM_default",
+                "NPM/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
